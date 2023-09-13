c2de06c6accefda15ffe8abf4f7506b4ac91707a
leopold <bowsersunterhose@gmail.com>
2023-09-13 15:49:24 +0200

    Initial commit

---
+++ app/src/main/java/at/ac/tgm/hit/sew7/lkernegger/calculator/MainActivity.java
@@ -0,0 +1,14 @@
+package at.ac.tgm.hit.sew7.lkernegger.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
